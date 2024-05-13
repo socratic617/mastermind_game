31be9cbaa41d09e2d916f82bc7f255a73b6d9bc1
socratic617 <erik.solis02135@gmail.com>
2024-05-12 20:04:42 -0400

    Added game Controller and authenticated

--- routes/main.js
+++ routes/main.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
-const homeController = require("../controllers/home");
+const gameController = require("../controllers/game");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
-router.get("/", homeController.getIndex); // (endpoint, callback function)
-router.get("/login", authController.getLogin);
+router.get("/game", ensureAuth, gameController.getGame);// (endpoint, callback function)
+router.get("/", authController.getLogin);
 router.post("/login", authController.postLogin);
 router.get("/logout", authController.logout);
 router.get("/signup", authController.getSignup);
 router.post("/signup", authController.postSignup);
+
 
 module.exports = router;
